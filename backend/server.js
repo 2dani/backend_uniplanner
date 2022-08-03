e7ff46092aea2bcc5927e73674b28973eb4fd2d7
Daeun Lee <daeunlee1993@gmail.com>
2022-08-03 18:29:12 +0200

    change upload3

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 //app.use(express.json())
-app.use(cors({origin: ["http://uniplannerbackend-env.eba-2mpxvpuu.us-east-1.elasticbeanstalk.com/"], credentials: true}))
+app.use(cors({origin: ["http://uniplannerbackend-env.eba-2mpxvpuu.us-east-1.elasticbeanstalk.com"], credentials: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
