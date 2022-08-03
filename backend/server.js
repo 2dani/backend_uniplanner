cb7d68b2a95e89137179df9e97a6a73c289e900d
Daeun Lee <daeunlee1993@gmail.com>
2022-08-03 18:22:14 +0200

    change upload

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 //app.use(express.json())
-app.use(cors())
+app.use(cors({origin: true, credentials: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
